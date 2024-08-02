9c009eaf7942359ae2350c9855128e8865c7900b
goyal-nandini <nandinigoyal1981@gmail.com>
2024-08-02 22:04:05 +0530

    Reviving code editor after two weeks  #lcm and hcf

---
+++ src/getBit.java
@@ -0,0 +1,18 @@
+// to get the bit at the given position, position is counted from right to left from 0,
+import java.util.*;
+public class getBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number in decimal form: ");
+        int number = sc.nextInt();
+        System.out.println("Enter the position of bit to get: ");
+        int pos = sc.nextInt();
+
+        int bitMask = 1 << pos;
+        if ((bitMask & number) == 0){
+            System.out.println("Bit at position " + pos + " is zero(0).");
+        } else {
+            System.out.println("Bit at position " + pos + " is one(1).");
+        }
+    }
+}
---
+++ src/setBit.java
@@ -0,0 +1,16 @@
+// to set the bit as 1 at the given position.
+import java.util.*;
+public class setBit {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number in decimal form: ");
+        int number = sc.nextInt();
+        System.out.println("Enter the position of bit to set: ");
+        int pos = sc.nextInt();
+
+        int bitMask = 1 << pos;
+        int newNumber = bitMask | number;
+        System.out.print("The new number after setting the bit at given position " + pos + " : ");
+        System.out.println(newNumber);
+    }
+}
